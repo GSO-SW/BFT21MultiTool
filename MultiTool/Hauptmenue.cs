06e4db60457cfbbbb9e5ae7e300f8175c212fee1
Julian Pankratz <julcrafter01@gmail.com>
2023-02-09 07:05:16 +0100

    .

--- MultiTool/Hauptmenue.cs
+++ MultiTool/Hauptmenue.cs
@@ -32,7 +32,7 @@ namespace MultiTool
                                   "                              >>> Hauptmenü <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
 
-                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");s
 
                 //Beschreibung der Software.
                 Console.WriteLine("\n\nMit der Software 'BFTMulti-Tool' sollen wiederkehrende oder besonders aufwendige\n" +
