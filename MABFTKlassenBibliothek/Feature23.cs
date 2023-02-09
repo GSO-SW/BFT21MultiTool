ada22e10d080507ce7c8e8b5eb3a8e55b9cdee52
Eray Alish <111420915+ErayAlish@users.noreply.github.com>
2023-02-09 11:12:39 +0100

    update Feature23

--- MABFTKlassenBibliothek/Feature23.cs
+++ MABFTKlassenBibliothek/Feature23.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +13,88 @@ namespace MABFTKlassenBibliothek
             Console.WriteLine("------------------------------------------------------------------------------------\n" +
                                   "                              >>> Quadratische Funktionen <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
+
+                double linearity;
+                double x1;
+                double y1;
+                double x2;
+                double y2;
+                double x3;
+                double y3;
+                double a;
+                double b;
+                double c;
+                double y;
+
+
+
+                do
+                {
+                    Console.WriteLine("1. Start");
+                    Console.WriteLine("2. Exit");
+                    Console.WriteLine("Choose an option (1 or 2): ");
+
+                    int option = int.Parse(Console.ReadLine());
+                    if (option == 1)
+                    {
+                        do
+                        {
+
+                            Console.WriteLine("Enter the first x coordinate: ");
+                            x1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the first y coordinate: ");
+                            y1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the second x coordinate: ");
+                            x2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the second y coordinate: ");
+                            y2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the third x coordinate: ");
+                            x3 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the third y coordinate: ");
+                            y3 = int.Parse(Console.ReadLine());
+
+                            // Linearity Test
+                            linearity = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+
+                            if (linearity == 0)
+                            {
+                                Console.WriteLine("Die 3 punkte sind linear.");
+                            }
+
+                        } while (linearity == 0);
+
+                        if (linearity != 0)
+                        {
+                            // Calculation of the function
+                            a = (x1 * (y2 - y3) + x2 * (y1 - y3) + x3 * (y3 - y1)) / linearity;
+                            b = (x1 * x1 * (y3 - y2) + x2 * x2 * (y1 - y3) + x3 * x3 * (y2 - y1)) / linearity;
+                            c = (x1 * x1 * (x2 * y3 - x3 * y2) + x2 * x2 * (x3 * y1 - x1 * y3) + x3 * x3 * (x1 * y2 - x2 * y1)) / linearity;
+
+                            Console.WriteLine("The quadratic function is: y = " + a + "x^2 + " + b + "x + " + c);
+
+                            // Table of solutions
+                            Console.WriteLine("Enter the starting value of x: ");
+                            int start = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the ending value of x: ");
+                            int end = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the increment value of x: ");
+                            int increment = int.Parse(Console.ReadLine());
+                            Console.WriteLine("x\ty");
+                            for (int x = start; x <= end; x += increment)
+                            {
+                                y = a * x * x + b * x + c;
+                                Console.WriteLine(x + "\t" + y);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+                } while (true);
+            
         }
     }
 }
