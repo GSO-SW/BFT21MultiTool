a9ca2d8b130100d6f223ebd364cba29d02bacf16
Muhammed Emir Köroglu <muhammedem.k@gso.schule.koeln>
2023-02-15 22:46:37 +0100

    - Polynomen wurde hinzugefügt und man kann es an einem Graphen sehen, wie es bei einem Koordinatensystem aussieht - MAMenue wurde bisschen angepasst und ein Feature wurde hinzugefügt, um eine leichte Bedienung beim auswählen zu haben

--- MABFTKlassenBibliothek/Feature21.cs
+++ MABFTKlassenBibliothek/Feature21.cs
@@ -14,6 +14,74 @@ namespace MABFTKlassenBibliothek
                                   "                              >>> Polynom <<<\n" +
                                   "------------------------------------------------------------------------------------\n\n");
 
+            Console.WriteLine("Geben Sie die Koeffizienten des Polynoms in aufsteigender Reihenfolge des Grades an: ");
+            string[] coefficients = Console.ReadLine().Split();
+            int[] polynomialCoefficients = new int[coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                polynomialCoefficients[i] = int.Parse(coefficients[i]);
+            }
+
+            Console.WriteLine("Geben Sie den Wert von x ein: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int result = CalculatePolynomialValue(polynomialCoefficients, x);
+            Console.WriteLine("Der Wert des Polynoms ist: " + result);
+
+            Console.WriteLine("\nDarstellung des Polynomgraphen:");
+            DrawCoordinateSystem();
+            DrawPolynomialGraph(polynomialCoefficients, x);
+            Console.ReadKey(true); 
+        }
+
+        static int CalculatePolynomialValue(int[] coefficients, int x)
+        {
+            int result = 0;
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                result += (int)Math.Pow(x, i) * coefficients[i];
+            }
+            return result;
+        }
+
+        static void DrawCoordinateSystem()
+        {
+            Console.WriteLine("+-------------+");
+            for (int i = 10; i >= -10; i--)
+            {
+                Console.Write("|");
+                for (int j = -10; j <= 10; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write("-");
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("+-------------+");
+        }
+
+        static void DrawPolynomialGraph(int[] coefficients, int x)
+        {
+            for (int i = -10; i <= 10; i++)
+            {
+                int y = CalculatePolynomialValue(coefficients, i);
+                Console.SetCursorPosition(10 + i, 10 - y);
+                Console.Write("*");
+            }
         }
     }
 }
--- MABFTKlassenBibliothek/MAMenue.cs
+++ MABFTKlassenBibliothek/MAMenue.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Threading.Channels;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
 
 namespace MABFTKlassenBibliothek
 {
@@ -8,7 +11,10 @@ namespace MABFTKlassenBibliothek
         public static void MASubMenue()
         {
             do {
+                int time = DateTime.Now.Hour;
                 Console.Title = "Mathematik";
+                ConsoleKeyInfo keyPressed = Console.ReadKey();
+
 
                 Console.Clear();
                 Console.WriteLine(@" /$$      /$$  /$$$$$$  /$$$$$$$$ /$$   /$$ /$$$$$$$$ /$$      /$$  /$$$$$$  /$$$$$$$$ /$$$$$$ /$$   /$$");
@@ -21,40 +27,65 @@ namespace MABFTKlassenBibliothek
                 Console.WriteLine(@"|__/     |__/|__/  |__/   |__/   |__/  |__/|________/|__/     |__/|__/  |__/   |__/   |______/|__/  \__/");
                 Thread.Sleep(1000);
 
-                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                Console.WriteLine("\n\n------------------------------------------------------------------------------------\n" +
                                    "                              >>> Mathematik <<<\n" +
                                    "------------------------------------------------------------------------------------\n\n");
 
+                if(time >= 0 && time < 12)
+                {
+                    Console.WriteLine("Guten Morgen! Es ist jetzt grade " + DateTime.Now.ToString("HH:mm") + " Uhr!\n");
+                }
+                else if(time >= 12 && time < 18)
+                {
+                    Console.WriteLine("Guten Nachmittag! Es ist jetzt grade " + DateTime.Now.ToString("HH:mm") + " Uhr!\n");
+                }
+                else
+                {
+                    Console.WriteLine("Guten Abend! Es ist jetzt grade " + DateTime.Now.ToString("HH:mm") + " Uhr!\n");
+                }
+
 
                 Console.WriteLine("----------Menü----------");
-                Console.WriteLine("1. Polynom");
-                Console.WriteLine("2. Flächenberechnung");
-                Console.WriteLine("3. Quadratische Funktionen");
-                Console.WriteLine("4. Lineare Funktionen");
-                Console.WriteLine("(Bitte wählen Sie irgendein Themenbereich)");
+                Console.WriteLine("1 -> Polynom");
+                Console.WriteLine("2 -> Flächenberechnung");
+                Console.WriteLine("3 -> Quadratische Funktionen");
+                Console.WriteLine("4 -> Lineare Funktionen");
+                Console.WriteLine("5 -> Zurück ins Hauptmenü");
+                Console.WriteLine("(Bitte wählen Sie irgendein Themenbereich mit deinen Pfeiltasten und drücke ENTER wenn du es gewählt hast)");
                 Console.WriteLine("------------------------");
                 Console.Write("> ");
-                int menu = Convert.ToInt32(Console.ReadLine());
+                string menu = Console.ReadLine();
 
-                if (menu == 1)
+                if (menu == "1")
                 {
                     Console.Clear();
                     Feature21.Polynom();
                 }
-                else if(menu == 2)
+                else if(menu == "2")
                 {
                     Console.Clear();
                     Feature22.flaechenberechnung();
                 }
-                else if(menu == 3)
+                else if(menu == "3")
                 {
                     Console.Clear();
                     Feature23.quadratischefunktionen();
                 }
-                else if(menu == 4)
+                else if(menu == "4")
                 {
                     Console.Clear();
                     Feature24.linearefunktionen();
+                }
+                else if (menu == "5")
+                {
+                    Console.Clear();
+                    break;
+                }
+                else if (menu.StartsWith("test"))
+
+                {
+                    Console.WriteLine("Comming Soon");
+                    Console.ReadKey();
                 }
             } while (true);
         }
