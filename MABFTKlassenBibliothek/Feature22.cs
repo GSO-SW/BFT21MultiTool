e59e12d57f0a4cb3f6be5fc13139f3781c6f9d82
Selo <sengulselahaddin@gmail.com>
2023-03-15 12:43:10 +0100

    menu weiter gemacht

--- MABFTKlassenBibliothek/Feature22.cs
+++ MABFTKlassenBibliothek/Feature22.cs
@@ -23,11 +23,69 @@ namespace MABFTKlassenBibliothek
                 "6 - Drachen\n" +
                 "7 - Raute\n" +
                 "8 - Drachenviereck\n" +
-                "9 - Kreis\n");
-            Console.ReadLine();
+                "9 - Kreis\n" +
+                "Tipp: mit der Eingabe des Wortes exit können sie das programm jederzeit verlassen.");
+            string Smenu = Console.ReadLine();
             
+            if (Smenu == "exit")
+            {
+                Environment.Exit(0);
+            }
+
+            int menu = Convert.ToInt32(Smenu);
+            if (menu == 1)
+            {
+                Console.WriteLine("Geben sie die Länge oder die Breite ein.");
+                string one = Console.ReadLine();
+                if (one == "exit")
+                {
+                    Environment.Exit(0);
+                }
+                int oneQ = Convert.ToInt32(one);
+                Console.WriteLine($"Die Fläche beträgt:{oneQ*oneQ}__²");
+            }
+            if (menu == 2)
+            {
+
+            }
+            if (menu == 3)
+            {
+
+            }
+            if (menu == 4)
+            {
+
+            }
+            if (menu == 5)
+            {
+
+            }
+            if (menu == 6)
+            {
+
+            }
+            if (menu == 7)
+            {
+
+            }
+            if (menu == 8)
+            {
+
+            }
+            if (menu == 9)
+            {
+
+            }
+            else if (menu <1 || menu >9 )
+            {
+                Console.WriteLine("Falsche Eingabe, bitte versuchen sie es nochmal");
+            }
 
 
+
+
+
+            Console.ReadLine();
         }
     }
 }
