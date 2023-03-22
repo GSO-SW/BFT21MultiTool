2ba516ed5e18335af7f7c7af68b1cecb7a79f1cd
Murtaza Behzad <murtaza.b@gso.schule.koeln>
2023-03-22 11:06:14 +0100

    Exit eingefügt

--- WIBFTKlassenBibliothek/Feature17.cs
+++ WIBFTKlassenBibliothek/Feature17.cs
@@ -15,8 +15,7 @@ namespace WIBFTKlassenBibliothek
             Exit= true;
 
 
-            if (Exit)
-             Environment.Exit(0);
+            
             double Kapitel = 0;
             double zinssatz = 0;
             double jahr = 0;
@@ -24,12 +23,57 @@ namespace WIBFTKlassenBibliothek
             double kontostand = 0;
             Console.WriteLine("Bitte geben sie ihr kapital ein");
 
-            Kapitel = Convert .ToDouble(Console.ReadLine());
-            Console.WriteLine("Bitte geben sie ihr Zinssatz ein");
-            zinssatz = Convert .ToDouble(Console.ReadLine());
+            string Kapitel_str = Console.ReadLine();
+            do {
+                if (Kapitel_str == "exit")
+                {
+                    Console.WriteLine("Programm wird beendet...");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Kapitel = Convert.ToDouble(Kapitel_str);
+
+                }
+            }
+            while (Kapitel==0 );
+
+                    Console.WriteLine("Bitte geben sie ihr Zinssatz ein");
+          string  zinssatz_str = Console.ReadLine();
+            do
+            {
+                if (zinssatz_str == "exit")
+                {
+                    Console.WriteLine("Programm wird beendet...");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    zinssatz  = Convert.ToDouble(zinssatz_str);
+
+                }
+            }
+            while (zinssatz  == 0);
+           
             Console.WriteLine("Bitte geben sie ihr anlagedauer ein");
-            jahr= Convert .ToDouble(Console.ReadLine());
-            
+           string  jahr_str= Console.ReadLine();
+            do
+            {
+                if (jahr_str == "exit")
+                {
+                    Console.WriteLine("Programm wird beendet...");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    jahr = Convert.ToDouble(jahr_str);
+
+                }
+            }
+            while (jahr == 0);
             gewinn = Kapitel*zinssatz* jahr;
 
             Console.WriteLine($"Ihr Gewinn beträgt:{gewinn}");
