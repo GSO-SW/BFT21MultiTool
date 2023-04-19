c06ceaf2b383d4c807afa0cc9d494f3d58c32003
Rahim Nikfar <rahim.n3@gso.schule.koeln>
2023-04-19 11:43:03 +0200

    ordnung update

--- ITBFTKlassenBibliothek/feat14.cs
+++ ITBFTKlassenBibliothek/feat14.cs
@@ -9,68 +9,48 @@ namespace ITBFTKlassenBibliothek
 {
     internal class Feature14
     {
-        ínternal static void Feature14()
+        public static void Feature_14()
         {
-            
+                        
+            Console.WriteLine("Willkommen zum DPI-Rechner!");
+            Console.WriteLine("Mit diesem Tool sind Sie in der Lage die dots per inch (DPI) zu Reschnen");
+            Console.WriteLine("Die DPI Reschnen Sie in dem Sie einmal Die Pixel in der Breite, sowie Höhe angeben und zu letzt Die Göße Ihres Bildschirms in Zoll");
+            Console.WriteLine();
 
-
-        {
-            static void Main(string[] args)
-
-
+            while (true)
             {
-                Console.WriteLine("Willkommen zum DPI-Rechner!");
-                Console.WriteLine("Mit diesem Tool sind Sie in der Lage die dots per inch (DPI) zu Reschnen");
-                Console.WriteLine("Die DPI Reschnen Sie in dem Sie einmal Die Pixel in der Breite, sowie Höhe angeben und zu letzt Die Göße Ihres Bildschirms in Zoll");
-                Console.WriteLine();
-
-
-
-
-
-
-
-                    while (true)
-                {
-                        Console.WriteLine("Gib die Breite deines Bildschirms in Pixeln ein:");
-                        int Breite = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gib die Breite deines Bildschirms in Pixeln ein:");
+                int Breite = Convert.ToInt32(Console.ReadLine());
                     
-                    if (Breite == -1) continue;
+            if (Breite == -1) continue;
 
-                        Console.WriteLine("Gib die Höhe deines Bildschirms in Pixeln ein:");
-                        int Höhe = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gib die Höhe deines Bildschirms in Pixeln ein:");
+                int Höhe = Convert.ToInt32(Console.ReadLine());
                     
-                    if (Höhe == -1) continue;
+            if (Höhe == -1) continue;
 
-                        Console.WriteLine("Gib die Größe deines Bildschirms in Zoll ein:");
-                        double size = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gib die Größe deines Bildschirms in Zoll ein:");
+                double size = Convert.ToInt32(Console.ReadLine());
                     
-                    if (size == -1) continue;
+            if (size == -1) continue;
 
-                    
                         
-                        
-                        
-                        double dpi = Math.Sqrt(Breite * Breite + Höhe * Höhe) / size;
-                    Console.WriteLine();
-                    Console.WriteLine("Deine DPI sind: " + dpi);
-                    Console.WriteLine();
+                double dpi = Math.Sqrt(Breite * Breite + Höhe * Höhe) / size;
+            Console.WriteLine();
+            Console.WriteLine("Deine DPI sind: " + dpi);
+            Console.WriteLine();
 
-                    Console.WriteLine("Möchtest du eine weitere Berechnung durchführen? (ja/nein)");
-                    string input = Console.ReadLine().ToLower();
-                    if (input == "nein")
-                    {
-                        break;
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.WriteLine("Vielen Dank, dass du den DPI-Rechner benutzt hast!");
+            Console.WriteLine("Möchtest du eine weitere Berechnung durchführen? (ja/nein)");
+            string input = Console.ReadLine().ToLower();
+            if (input == "nein")
+            {
+                break;
+            }
+            Console.WriteLine();
             }
 
-
-
-
+            Console.WriteLine("Vielen Dank, dass du den DPI-Rechner benutzt hast!");
+            
 
             static int GetIntegerInput(string prompt)
             {
@@ -87,12 +67,6 @@ namespace ITBFTKlassenBibliothek
                     return value;
                 }
             }
-
-
-
-
-
-
 
             static double GetDoubleInput(string prompt)
             {
@@ -111,7 +85,7 @@ namespace ITBFTKlassenBibliothek
             }
         }
     }
-    }
+    
 }
 
 
