d5c43827697c555a74a69b46ff48b2039b301ea7
ErayAlish <111420915+ErayAlish@users.noreply.github.com>
2023-04-23 21:29:30 +0200

    F 23

--- MABFTKlassenBibliothek/Feature23.cs
+++ MABFTKlassenBibliothek/Feature23.cs
@@ -18,7 +18,7 @@ namespace MABFTKlassenBibliothek
 
             // Variablendeklaration
             double x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, a, b, c, y, x, linearity;
-            bool flag1, Validinput1 = true, Validinput2 = true, Validinput3 = true, Validinput4 = true, Validinput5 = true, Validinput6 = true, Validinput7 = true;
+            bool flag1, Validinput1 = true, Validinput2 = true, Validinput3 = true, Validinput4 = true, Validinput5 = true, Validinput6 = true;
             int menu;
             string input1, input2, input3, input4, input5, input6;
 
@@ -30,11 +30,9 @@ namespace MABFTKlassenBibliothek
                 //Menu
                 Console.WriteLine("1. Start");
                 Console.WriteLine("2. Exit");
-                Console.WriteLine("Wählen Sie bitte eine Option (1 oder 2): ");
-                // Überprüfung der Benutzereingabe
-                flag1 = (int.TryParse(Console.ReadLine(), out menu));
-                // Überprüft der Benutzereingabe menu kleiner als 1
-                if (flag1 == true && menu <1 )
+                Console.WriteLine("Wählen Sie bitte eine Option (1 oder 2): ");               
+                flag1 = (int.TryParse(Console.ReadLine(), out menu)); // Überprüfung der Benutzereingabe
+                if (flag1 == true && menu <1) // Überprüft der Benutzereingabe menu kleiner als 1
                 {
                     Console.WriteLine("Falsche Eingabe");
                     Console.WriteLine("Drücken um neu einzugeben");
@@ -61,8 +59,9 @@ namespace MABFTKlassenBibliothek
                 {
                     do
                     {
-                        do{
-                            Console.WriteLine("Gib den erste x Koordinat ein : "); // Benutzereingabe für x1
+                        do
+                        {
+                            Console.Write("Gib den erste x Koordinat ein : "); // Benutzereingabe für x1
                             input1 = Console.ReadLine();
                             if (input1.ToLower() == "exit")   // Überprüfung ob die Benutzereingabe "exit" ist
                             {
@@ -70,7 +69,7 @@ namespace MABFTKlassenBibliothek
                                 Thread.Sleep(2300);
                                 Environment.Exit(0);
                                 break;
-                                 
+
                             }
 
                             while (!double.TryParse(input1, out x1))  // Überprüfung ob die Eingabe eine Zahl ist
@@ -86,7 +85,8 @@ namespace MABFTKlassenBibliothek
 
                         } while (!Validinput1);
 
-                        do{
+                        do
+                        {
                             Console.WriteLine("Gib den erste y Koordinat ein : "); // Benutzereingabe für y1
                             input2 = Console.ReadLine();
                             if (input2.ToLower() == "exit")  // Überprüfung ob die Benutzereingabe "exit" ist
@@ -103,14 +103,15 @@ namespace MABFTKlassenBibliothek
                                 Console.WriteLine("Falsche Eingabe");
                                 Console.WriteLine("Drücken um neu einzugeben");
                                 Console.ReadKey();
-                                Console.Clear();
+
                                 break;
 
                             }
 
                         } while (!Validinput2);
 
-                       do {
+                        do
+                        {
                             Console.WriteLine("Gib den zweite x Koordinat ein : "); // Benutzereingabe für x2
                             input3 = Console.ReadLine();
                             if (input3.ToLower() == "exit")   // Überprüfung ob die Benutzereingabe "exit" ist
@@ -131,31 +132,33 @@ namespace MABFTKlassenBibliothek
 
                             }
 
-                       } while (!Validinput3);
+                        } while (!Validinput3);
 
-                       do{ 
-                           Console.WriteLine("Gib den zweite y Koordinat ein : "); // Benutzereingabe für y2
-                           input4 = Console.ReadLine();
-                           if (input4.ToLower() == "exit")   // Überprüfung ob die Benutzereingabe "exit" ist
-                           {
-                               Console.WriteLine("Konsole wird Beedndet.");
-                               Thread.Sleep(2300);
-                               Environment.Exit(0);
-                           }
-                           while (!double.TryParse(input4, out y2))  // Überprüfung ob die Eingabe eine Zahl ist
-                           {
+                        do
+                        {
+                            Console.WriteLine("Gib den zweite y Koordinat ein : "); // Benutzereingabe für y2
+                            input4 = Console.ReadLine();
+                            if (input4.ToLower() == "exit")   // Überprüfung ob die Benutzereingabe "exit" ist
+                            {
+                                Console.WriteLine("Konsole wird Beedndet.");
+                                Thread.Sleep(2300);
+                                Environment.Exit(0);
+                            }
+                            while (!double.TryParse(input4, out y2))  // Überprüfung ob die Eingabe eine Zahl ist
+                            {
                                 Validinput4 = false;
                                 Console.WriteLine("Falsche Eingabe");
-                               Console.WriteLine("Drücken um neu einzugeben");
-                               Console.ReadKey();
-                               Console.Clear();
-                               break;
+                                Console.WriteLine("Drücken um neu einzugeben");
+                                Console.ReadKey();
+                                Console.Clear();
+                                break;
 
-                           }
+                            }
 
-                       } while (!Validinput4) ;
+                        } while (!Validinput4);
 
-                       do{
+                        do
+                        {
                             Console.WriteLine("Gib den Dritte x Koordinat ein : "); // Benutzereingabe für x3
                             input5 = Console.ReadLine();
                             if (input5.ToLower() == "exit")  // Überprüfung ob die Benutzereingabe "exit" ist
@@ -177,29 +180,30 @@ namespace MABFTKlassenBibliothek
 
                             }
 
-                       } while (!Validinput5);
+                        } while (!Validinput5);
 
-                       do{
-                           Console.WriteLine("Gib den dritten y Koordinat ein:"); // Benutzereingabe für y3
-                           input6 = Console.ReadLine();
+                        do
+                        {
+                            Console.WriteLine("Gib den dritten y Koordinat ein:"); // Benutzereingabe für y3
+                            input6 = Console.ReadLine();
 
-                           if (input6.ToLower() == "exit") ;   // Überprüfung ob die Benutzereingabe "exit" ist
-                           {
-                               Environment.Exit(0);
-                               break;
+                            if (input6.ToLower() == "exit") ;   // Überprüfung ob die Benutzereingabe "exit" ist
+                            {
+                                Environment.Exit(0);
+                                break;
 
-                           }
-                           while (!double.TryParse(input6, out y3))  // Überprüfung ob die Eingabe eine Zahl ist
-                           {
-                               Validinput6 = false;
-                               Console.WriteLine("Falsche Eingabe");
-                               Console.WriteLine("Drücken um neu einzugeben");
-                               Console.ReadKey();
-                               Console.Clear();
-                               break;
-                           }   
-                            
-                       } while (!Validinput6);
+                            }
+                            while (!double.TryParse(input6, out y3))  // Überprüfung ob die Eingabe eine Zahl ist
+                            {
+                                Validinput6 = false;
+                                Console.WriteLine("Falsche Eingabe");
+                                Console.WriteLine("Drücken um neu einzugeben");
+                                Console.ReadKey();
+                                Console.Clear();
+                                break;
+                            }
+
+                        } while (!Validinput6);
 
 
                         // Linearlität test
@@ -214,6 +218,7 @@ namespace MABFTKlassenBibliothek
                         }
 
 
+
                         if (linearity != 0) // Wenn Benutzereingabe nicht Linear ist
                         {
                             // Berechnung der Funktion
@@ -223,12 +228,24 @@ namespace MABFTKlassenBibliothek
 
                             Console.WriteLine("Der Quadratische Funktion ist: y = " + a + "x^2 + " + b + "x + " + c);
                             Thread.Sleep(3500);
-                    
+                            Console.WriteLine("Drücke um neu Einzugeben oder beedende das Konsole mit (Exit).");
+                            string menu2 = Console.ReadLine();
+                            if (menu2.ToLower() == "exit")   // Überprüfung ob die Benutzereingabe "exit" ist
+                            {
+                                Console.WriteLine("Konsole wird Beedndet.");
+                                Thread.Sleep(2300);
+                                Environment.Exit(0);
+                                break;
+
+                            }
+
+
                         }
+
 
                     } while (linearity == 0); // wenn der Funktion Linear ist wird es zu menu geleitet
 
-                }
+                } 
                 else if (menu == 2)// wenn Benutzereingabe "2" ist 
                 {
                     Environment.Exit(0);
