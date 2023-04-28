1b17f6e071e4b0e47f5db08982a2740e7c3c236b
Julian Pankratz <109351765+Julcraftlp@users.noreply.github.com>
2023-04-28 23:27:51 +0200

    Update Program.cs

--- MultiTool/Program.cs
+++ MultiTool/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
-using WIBFTKlassenBibliothek;
+using System;
+
+
+//Die Größe des Konsolenfensters wird an den Bildschirm angepasst.
+Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+//Schriftfarbe wird auf Grün gesetzt.
 Console.ForegroundColor = ConsoleColor.Green;
-WIMenue.WISubMenue();
+
+//Der Schrifthintergrund kann geändert werden.
+//Console.BackgroundColor = ConsoleColor.DarkRed;
+
+//Aufruf des Hauptmenüs.
+MultiTool.Hauptmenue.HauptmenueAufruf();
