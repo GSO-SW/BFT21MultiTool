5c5b33565e20a1c6633d6240004f33fa6eb0fecf
Julian Pankratz <109351765+Julcraftlp@users.noreply.github.com>
2023-04-28 23:15:23 +0200

    Create Feature17.cs

---
+++ WIBFTKlassenBibliothek/Feature17.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIBFTKlassenBibliothek
+{
+    internal class Feature17
+    {
+
+    public static void run()
+        {
+
+        }
+
+    }
+}
