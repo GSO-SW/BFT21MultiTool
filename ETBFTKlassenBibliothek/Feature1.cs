8f7278ad8ca873f88e1d119e44a46cfbed082ba2
Alexander Skibbe <Alexander Skibbe@LAPTOP-G9BKHLKS>
2023-05-03 09:12:26 +0200

    84184

--- ETBFTKlassenBibliothek/Feature1.cs
+++ ETBFTKlassenBibliothek/Feature1.cs
@@ -12,96 +12,157 @@ namespace ETBFTKlassenBibliothek
         {
             Console.ForegroundColor = ConsoleColor.Blue;
 
-            double spannung = 0, widerstand = 0, zeit = 0;
-            double strom = 0, energie = 0, ladung = 0;
+            double spannung = 0, widerstand = 0, zeit = 0; double strom = 0, energie = 0, ladung = 0; string sspannung;
+            string wwiderstand; string zzeit;
+
 
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Was möchtest du berechnen? Gib eine der folgenden Optionen ein:\n\n" +
-                                  "1. Stromstärke\n\n" +
-                                  "2. Energieverbrauch\n\n" +
-                                  "3. Ladungsmenge in Zeit\n\n" +
-                                  "Oder gib 'exit' zum Beenden ein.");
-
-                string auswahl = Console.ReadLine();
-
+                Console.WriteLine("Was  möchtest  du  berechnen?  Gib  eine  der  folgenden  Optionen  ein:\n\n" + "1.  Stromstärke\n\n" +
+                "2. Energieverbrauch\n\n" + "3.  Ladungsmenge  in  Zeit\n\n" +
+                "gib  'exit'  zum  Beenden  ein.\n\n" +
+                "oder  gib  'subexit'  ein,  um  ins  Hauptmenue  zu  kommen."); string auswahl = Console.ReadLine();
                 if (auswahl == "exit")
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Beende das Programm");
+                    Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Beende  das  Programm"); Environment.Exit(0);
                     break;
+                }
+                else if (auswahl == "subexit")
+                {
+                    ETBFTMenue.ETMenue(); break;
                 }
 
                 Console.Clear();
-                Console.WriteLine("Bitte gib die folgenden Werte ein:\n");
+                Console.WriteLine("Bitte  gib  die  folgenden  Werte  ein:\n");
 
                 try
                 {
-                    Console.Write("Spannung in Volt: ");
-                    spannung = double.Parse(Console.ReadLine());
+                    Console.Write("Spannung  in  Volt:  \n\n" +
+                    "gib  'exit'  zum  Beenden  ein.\n\n" +
+                    "oder  gib  'subexit'  ein,  um  ins  Hauptmenue  zu  kommen.");
+
+
+                    sspannung = (Console.ReadLine()); if (sspannung == "exit")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine();
+                        Environment.Exit(0);
+                    }
+                    else if (sspannung == "subexit")
+                    {
+                        ETBFTMenue.ETMenue(); break;
+                    }
+                    spannung = double.Parse(sspannung);
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl ein.");
-                    Console.ReadKey();
+
+
+
+
+                }
+                try
+                {
+
+                    Console.WriteLine("Ungültige  Eingabe.  Bitte  gib  eine  Zahl  ein."); Console.ReadKey();
                     continue;
-                }
 
-                try
-                {
-                    Console.Write("Widerstand in Ohm: ");
-                    widerstand = double.Parse(Console.ReadLine());
+
+
+                    Console.Write("Widerstand  in  Ohm:  \n\n" +
+                    "gib  'exit'  zum  Beenden  ein.\n\n" +
+                    "oder  gib  'subexit'  ein,  um  ins  Hauptmenue  zu  kommen.");
+
+
+                    wwiderstand = (Console.ReadLine()); if (wwiderstand == "exit")
+                    {
+
+                        Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine();
+                        Environment.Exit(0);
+                    }
+                    else if (wwiderstand == "subexit")
+                    {
+                        ETBFTMenue.ETMenue(); break;
+                    }
+                    widerstand = double.Parse(wwiderstand);
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl ein.");
-                    Console.ReadKey();
+                    Console.WriteLine("Ungültige  Eingabe.  Bitte  gib  eine  Zahl  ein."); Console.ReadKey();
                     continue;
                 }
 
                 if (auswahl == "1")
                 {
-                    strom = spannung / widerstand;
-                    Console.WriteLine("\nStromstärke: {0} Ampere", strom);
+                    strom = spannung / widerstand; Console.WriteLine("\nStromstärke:  {0}  Ampere", strom);
                 }
                 else if (auswahl == "2")
                 {
-                    strom = spannung / widerstand;
-                    energie = spannung * strom;
-                    Console.WriteLine("\nEnergieverbrauch: {0} Joule", energie);
+                    strom = spannung / widerstand; energie = spannung * strom;
+                    Console.WriteLine("\nEnergieverbrauch:  {0}  Joule", energie);
                 }
                 else if (auswahl == "3")
                 {
+
                     try
                     {
-                        Console.Write("Zeit in Sekunden: ");
-                        zeit = double.Parse(Console.ReadLine());
+
+
+
+                        Console.Write("Zeit  in  Sekunden:  \n\n" +
+                        "gib  'exit'  zum  Beenden  ein.\n\n" +
+                        "oder  gib  'subexit'  ein,  um  ins  Hauptmenue  zu  kommen.");
+
+
+                        zzeit = (Console.ReadLine()); if (zzeit == "exit")
+                        {
+
+                            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine();
+                            Environment.Exit(0);
+
+                        }
+                        else if (zzeit == "subexit")
+                        {
+                            ETBFTMenue.ETMenue(); break;
+                        }
+                        zeit = double.Parse(zzeit);
                     }
+
                     catch (FormatException)
                     {
-                        Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl ein.");
-                        Console.ReadKey();
+                        Console.WriteLine("Ungültige  Eingabe.  Bitte  gib  eine  Zahl  ein."); Console.ReadKey();
                         continue;
                     }
 
-                    strom = spannung / widerstand;
-                    ladung = strom * zeit;
-                    Console.WriteLine("\nLadungsmenge: {0} Coulomb", ladung);
-                }
-                else
-                {
-                    Console.WriteLine("\nUngültige Eingabe. Bitte versuche es erneut.");
-                    continue;
+
+
+
+
                 }
 
-                Console.WriteLine("\nDrücke eine beliebige Taste, um fortzufahren.");
-                Console.ReadKey();
+
+                else
+                {
+
+
+                }
+
+
+                strom = spannung / widerstand; ladung = strom * zeit;
+                Console.WriteLine("\nLadungsmenge:  {0}  Coulomb", ladung);
+
+
+
+
+
+                Console.WriteLine("\nUngültige  Eingabe.  Bitte  versuche  es  erneut."); continue;
+
+
+                Console.WriteLine("\nDrücke  eine  beliebige  Taste,  um  fortzufahren."); Console.ReadKey();
                 Console.SetCursorPosition(0, Console.CursorTop - 1);
             }
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green; Console.ReadLine();
         }
     }
 }
